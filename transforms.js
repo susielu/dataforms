e4f77ccfcdad10b64feb750d3a2e019d11230fb3
Susie Lu <Susie Lu>
2017-07-27 11:53:35 -0700

    initial transforms

---
+++ transforms.js
@@ -0,0 +1,90 @@
+function toMapByDimension({ values, dim, getValue = d => d }) {
+  const dimensions = {}
+  values.forEach(d => {
+    if (!dimensions[d[dim]]) {
+      dimensions[d[dim]] = []
+    }
+    dimensions[d[dim]].push(getValue(d))
+  })
+  return dimensions
+}
+
+//would be nice to handle deltas, significance testing, etc transformations
+//7dma functionality
+
+//would also be nice to be able to add in stats on each point
+function toMappedLinesByDimension({ mapObject, stats }) {
+  return Object.keys(mapObject).map(k => {
+    const line = {
+      name: k,
+      coordinates: mapObject[k],
+      stats: {}
+    }
+
+    if (stats) {
+      line.stats = stats.transform(line.coordinates)
+    }
+    return line
+  })
+}
+
+function sortByTimestamp(dim) {
+  return (a, b) => {
+    return new Date(a[dim]) - new Date(b[dim])
+  }
+}
+
+//dim can be a function or a string otherwise
+//assume it is an array of timestamps
+function timestampsFromData({ values, dim }) {
+  const times = new Map()
+  const dimAccessor = !dim
+    ? d => d
+    : typeof dim === "function" ? dim : d => d[dim]
+
+  values.forEach(d => {
+    times.set(dimAccessor(d), true)
+  })
+
+  return [...times.keys()]
+}
+
+//Moves through the array of values assuming the values are sorted by timestamp
+//metrics is an array of metric names
+//If no value, then creates a filled object with the array of metrics
+//and pads the metrics as zero
+
+//this function does not deal with mismatched timestamps
+//   out of order, missing values in timestamps array
+function zeroFillLine({
+  values,
+  timestamps,
+  metrics,
+  timeDim = "timestamp",
+  zeroFill = 0
+}) {
+  if (values.length === timestamps.length) {
+    return values
+  }
+  timestamps.forEach((d, i) => {
+    if (values.map(p => p[timeDim]).indexOf(d) === -1) {
+      const emptyFillObject = Object.assign({}, values[0])
+      Object.keys(emptyFillObject).forEach(p => {
+        if (metrics.indexOf(p) !== -1) {
+          emptyFillObject[p] = zeroFill
+        }
+      })
+      emptyFillObject[timeDim] = d
+      values.splice(i, 0, emptyFillObject)
+    }
+  })
+
+  return values
+}
+
+module.exports = {
+  toMapByDimension,
+  sortByTimestamp,
+  zeroFillLine,
+  timestampsFromData
+}
---
+++ transforms.test.js
@@ -0,0 +1,193 @@
+const transforms = require("./transforms")
+
+const values = ["2017-02-01", "2017-02-03", "2017-02-04", "2017-02-07"]
+
+describe("toMapByDimension", () => {
+  test("turns into dimension", () => {
+    const values = [
+      {
+        signup_cnt: 901,
+        subregion: "Africa",
+        timestamp: "2017-04-24T00:00:00.000Z"
+      },
+      {
+        signup_cnt: 8790,
+        subregion: "Australia/NZ",
+        timestamp: "2017-04-24T00:00:00.000Z"
+      },
+      {
+        signup_cnt: 901,
+        subregion: "Africa",
+        timestamp: "2017-04-25T00:00:00.000Z"
+      },
+      {
+        signup_cnt: 8790,
+        subregion: "Australia/NZ",
+        timestamp: "2017-04-25T00:00:00.000Z"
+      }
+    ]
+
+    const transformed = {
+      Africa: [
+        {
+          signup_cnt: 901,
+          subregion: "Africa",
+          timestamp: "2017-04-24T00:00:00.000Z"
+        },
+        {
+          signup_cnt: 901,
+          subregion: "Africa",
+          timestamp: "2017-04-25T00:00:00.000Z"
+        }
+      ],
+      "Australia/NZ": [
+        {
+          signup_cnt: 8790,
+          subregion: "Australia/NZ",
+          timestamp: "2017-04-24T00:00:00.000Z"
+        },
+        {
+          signup_cnt: 8790,
+          subregion: "Australia/NZ",
+          timestamp: "2017-04-25T00:00:00.000Z"
+        }
+      ]
+    }
+
+    expect(transforms.toMapByDimension({ values, dim: "subregion" })).toEqual(
+      transformed
+    )
+  })
+})
+
+describe("timestampsFromData", () => {
+  test("array values", () => {
+    expect(transforms.timestampsFromData({ values })).toEqual(values)
+  })
+
+  test("duplicate values", () => {
+    const duplicateValues = ["2017-02-01", "2017-02-01", "2017-02-01"]
+
+    expect(transforms.timestampsFromData({ values: duplicateValues })).toEqual([
+      "2017-02-01"
+    ])
+  })
+
+  test("object values", () => {
+    const objectValues = [
+      { date: "2017-02-01" },
+      { date: "2017-02-03" },
+      { date: "2017-02-04" },
+      { date: "2017-02-07" }
+    ]
+
+    expect(
+      transforms.timestampsFromData({ values: objectValues, dim: "date" })
+    ).toEqual(values)
+  })
+
+  test("function values", () => {
+    const objectValues = [
+      { date: "2017-02-01" },
+      { date: "2017-02-03" },
+      { date: "2017-02-04" },
+      { date: "2017-02-07" }
+    ]
+
+    expect(
+      transforms.timestampsFromData({ values: objectValues, dim: d => d.date })
+    ).toEqual(values)
+  })
+})
+
+describe("zeroFillLine", () => {
+  const values = [
+    { timestamp: "2017-02-01", metric: 5 },
+    { timestamp: "2017-02-03", metric: 4 },
+    { timestamp: "2017-02-04", metric: 3 },
+    { timestamp: "2017-02-07", metric: 1 }
+  ]
+
+  const timestamps = [
+    "2017-01-31",
+    "2017-02-01",
+    "2017-02-03",
+    "2017-02-04",
+    "2017-02-05",
+    "2017-02-06",
+    "2017-02-07",
+    "2017-02-08"
+  ]
+
+  const zeroFilled = [
+    { metric: 0, timestamp: "2017-01-31" },
+    { metric: 5, timestamp: "2017-02-01" },
+    { metric: 4, timestamp: "2017-02-03" },
+    { metric: 3, timestamp: "2017-02-04" },
+    { metric: 0, timestamp: "2017-02-05" },
+    { metric: 0, timestamp: "2017-02-06" },
+    { metric: 1, timestamp: "2017-02-07" },
+    { metric: 0, timestamp: "2017-02-08" }
+  ]
+
+  test("basic", () => {
+    const metrics = ["metric"]
+
+    expect(transforms.zeroFillLine({ values, timestamps, metrics })).toEqual(
+      zeroFilled
+    )
+  })
+
+  test("different time dimension", () => {
+    const values = [
+      { date: "2017-02-01", metric: 5 },
+      { date: "2017-02-03", metric: 4 },
+      { date: "2017-02-04", metric: 3 },
+      { date: "2017-02-07", metric: 1 }
+    ]
+
+    const metrics = ["metric"]
+
+    const zeroFilled = [
+      { metric: 0, date: "2017-01-31" },
+      { metric: 5, date: "2017-02-01" },
+      { metric: 4, date: "2017-02-03" },
+      { metric: 3, date: "2017-02-04" },
+      { metric: 0, date: "2017-02-05" },
+      { metric: 0, date: "2017-02-06" },
+      { metric: 1, date: "2017-02-07" },
+      { metric: 0, date: "2017-02-08" }
+    ]
+
+    expect(
+      transforms.zeroFillLine({ values, timestamps, metrics, timeDim: "date" })
+    ).toEqual(zeroFilled)
+  })
+
+  test("additional metrics", () => {
+    const metrics = ["metric", "count"]
+    const values = [
+      { timestamp: "2017-02-01", metric: 5, cell: 10 },
+      { timestamp: "2017-02-03", metric: 4, cell: 10 },
+      { timestamp: "2017-02-04", metric: 3, cell: 10 },
+      { timestamp: "2017-02-07", metric: 1, cell: 10 }
+    ]
+
+    const zeroFilled = [
+      { metric: 0, timestamp: "2017-01-31", cell: 10 },
+      { metric: 5, timestamp: "2017-02-01", cell: 10 },
+      { metric: 4, timestamp: "2017-02-03", cell: 10 },
+      { metric: 3, timestamp: "2017-02-04", cell: 10 },
+      { metric: 0, timestamp: "2017-02-05", cell: 10 },
+      { metric: 0, timestamp: "2017-02-06", cell: 10 },
+      { metric: 1, timestamp: "2017-02-07", cell: 10 },
+      { metric: 0, timestamp: "2017-02-08", cell: 10 }
+    ]
+
+    expect(transforms.zeroFillLine({ values, timestamps, metrics })).toEqual(
+      zeroFilled
+    )
+  })
+
+  //test("prepost", () => {})
+})
